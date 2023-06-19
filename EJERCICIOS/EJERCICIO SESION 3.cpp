961a09002ef7cb9112d5bb605b209f1235fb4a8c
Erick Tut <87912114+Erick-Tut@users.noreply.github.com>
2023-06-19 17:00:58 -0600

    Update EJERCICIO SESION 3.cpp

--- EJERCICIOS/EJERCICIO SESION 3.cpp
+++ EJERCICIOS/EJERCICIO SESION 3.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int main (){
 	
-
 	int numero;    
 	
 	cout << "Ingrese cuantos nombres quiere utilizar: " << endl;
@@ -17,14 +16,12 @@ int main (){
 	
 	cout << "Ingrese nombres: " << endl;
 	
-		
-	
- for(int i=0; i<= numero; i++){
+ for(int i = 0; i<= numero; i++){
 	
 	cin.getline(nombre[i], 20, '\n'); 
 	
  }
- for(int i=1; i<= numero; i++){
+ for(int i = 1; i<= numero; i++){
  	
 	int distancia = strlen(nombre[i]);
 	
